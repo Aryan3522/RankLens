e3d5b18b7bc87640ea668012419f3e9cc21aec17
Aryan <aryanhooda3722@gmail.com>
2026-05-09 00:04:40 +0530

    chore: implement enterprise-level CORS defense and resolve startup crashes

--- server/src/app.ts
+++ server/src/app.ts
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", origin);
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept");
+    res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization");
   }
 
   if (req.method === "OPTIONS") {
